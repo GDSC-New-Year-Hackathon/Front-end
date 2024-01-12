013a43795da64108ddeb24e91e1790253e87fe1e
eqypo9 <eqypo@naver.com>
2024-01-13 01:16:57 +0900

    Committing local changes before pulling from main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import './App.css';
+import Calendar from "./component/Calendar.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Calendar />
     </div>
   );
 }
 
 export default App;
+
+
+// import ReactDOM from 'react-dom';
+
+// const rootElement = document.getElementById('root');
+
+// ReactDOM.createRoot(rootElement).render(<App />);
+
+
+// class Subject extends Component {
+//   render() {
+//     return (
+//       <header>
+//         <h1>calender</h1>
+//       </header>
+//     );
+//   }
+// }
+
+// class TOC extends Component {
+//   render() {
+//     return(
+//       <nav>
+//         <ul>
+//           <li>테스트</li>
+//           <li>테스트</li>
+//           <li>테스트</li>
+//         </ul>
+//       </nav>
+//     )
+//   }
+// }
+
+// class Content extends Component {
+//   render() {
+//     return(
+//       <article>
+//         <h2>달성률을 캘린더에서 확인해보세요.</h2>
+//       </article>
+//     )
+//   }
+// }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <Subject title="CAL" sub="calender"></Subject>
+//         <TOC></TOC>
+//         <Content></Content>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App; 
